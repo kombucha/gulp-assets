8328280c9daea88a96b545bf3557c8ab0e3ccc73
Vincent Lemeunier <vincent.lemeunier+git@gmail.com>
2014-12-26 11:13:26 +0100

    fix: Updated @Zweer tests to match the new api of should.js

--- test/main.js
+++ test/main.js
@@ -100,8 +100,8 @@ describe("gulp-assets", function () {
         });
 
         stream.on("end", function () {
-            javascriptFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/js/bar.js');
+            javascriptFiles.should.have.lengthOf(1);
+            javascriptFiles.should.containEql('test/fixtures/js/bar.js');
             done();
         });
 
@@ -130,8 +130,8 @@ describe("gulp-assets", function () {
         });
 
         stream.on("end", function () {
-            javascriptFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/js/bar.js');
+            javascriptFiles.should.have.lengthOf(1);
+            javascriptFiles.should.containEql('test/fixtures/js/bar.js');
             done();
         });
 
@@ -164,7 +164,7 @@ describe("gulp-assets", function () {
 
         stream.on("end", function () {
             javascriptFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/js/foo.js');
+            javascriptFiles.should.containEql('test/fixtures/js/foo.js');
             done();
         });
 
@@ -194,7 +194,7 @@ describe("gulp-assets", function () {
 
         stream.on("end", function () {
             javascriptFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/js/foo.js');
+            javascriptFiles.should.containEql('test/fixtures/js/foo.js');
             done();
         });
 
@@ -292,7 +292,7 @@ describe("gulp-assets", function () {
 
         stream.on("end", function () {
             cssFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/css/bar.css');
+            cssFiles.should.containEql('test/fixtures/css/bar.css');
             done();
         });
 
@@ -322,7 +322,7 @@ describe("gulp-assets", function () {
 
         stream.on("end", function () {
             cssFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/css/bar.css');
+            cssFiles.should.containEql('test/fixtures/css/bar.css');
             done();
         });
 
@@ -355,7 +355,7 @@ describe("gulp-assets", function () {
 
         stream.on("end", function () {
             cssFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/css/foo.css');
+            cssFiles.should.containEql('test/fixtures/css/foo.css');
             done();
         });
 
@@ -384,8 +384,8 @@ describe("gulp-assets", function () {
         });
 
         stream.on("end", function () {
-            cssFiles.should.have.lengthOf(1)
-                .and.contain('test/fixtures/css/foo.css');
+            cssFiles.should.have.lengthOf(1);
+            cssFiles.should.containEql('test/fixtures/css/foo.css');
             done();
         });
 
@@ -395,37 +395,37 @@ describe("gulp-assets", function () {
 
     it("should find the css file even if they are in another path", function (done) {
 
-      var srcFile = new gutil.File({
-          path: "test/fixtures/foo.html",
-          cwd: "test/",
-          base: "test/fixtures",
-          contents: fs.readFileSync("test/fixtures/foo.html")
-        }),
-        stream = assets({
-            js: false,
-            css: true,
-            cwd: '../fixtures_2'
-        }),
-        cssFiles = [];
+        var srcFile = new gutil.File({
+                path: "test/fixtures/foo.html",
+                cwd: "test/",
+                base: "test/fixtures",
+                contents: fs.readFileSync("test/fixtures/foo.html")
+            }),
+            stream = assets({
+                js: false,
+                css: true,
+                cwd: '../fixtures_2'
+            }),
+            cssFiles = [];
 
-      stream.on("error", function (err) {
-        should.exist(err);
-        done(err);
-      });
+        stream.on("error", function (err) {
+            should.exist(err);
+            done(err);
+        });
 
-      stream.on("data", function (newFile) {
-        cssFiles.push(newFile.path);
-      });
+        stream.on("data", function (newFile) {
+            cssFiles.push(newFile.path);
+        });
 
-      stream.on("end", function () {
-        cssFiles.should.have.lengthOf(2)
-          .and.contain('test/fixtures_2/css/foo.css')
-          .and.contain('test/fixtures_2/css/bar.css');
-        done();
-      });
+        stream.on("end", function () {
+            cssFiles.should.have.lengthOf(2)
+            cssFiles.should.containEql('test/fixtures_2/css/foo.css')
+            cssFiles.should.containEql('test/fixtures_2/css/bar.css');
+            done();
+        });
 
-      stream.write(srcFile);
-      stream.end();
+        stream.write(srcFile);
+        stream.end();
     });
 
     it("should error on stream", function (done) {
